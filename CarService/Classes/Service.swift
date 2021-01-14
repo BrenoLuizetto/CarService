38c5aef71f3b0636c02225f28c7d30c7f9f09854
Breno <cintrabreno22@gmail.com>
2021-01-14 15:36:14 -0300

    big bang

---
+++ CarService/Classes/Service.swift
@@ -0,0 +1,33 @@
+//
+//  Service.swift
+//  CarService
+//
+//  Created by Breno Luizetto Cintra on 14/01/21.
+//
+
+import Foundation
+public enum MyError: String, Error {
+    case carNotFounded = "nenhum carro foi encontrado"
+}
+
+public struct CarModel {
+    var model: String
+    var color: String
+}
+ public protocol ServiceCarsProtocol{
+    func getCars(completionHandler: (Result<[CarModel], MyError>) -> Void)
+}
+
+
+public class ServiceCars: ServiceCarsProtocol{
+    
+    public  init(){
+        
+    }
+    
+    public  func getCars(completionHandler: (Result<[CarModel], MyError>) -> Void){
+        let cars = [CarModel(model: "KA", color: "Branco"),
+                    CarModel(model: "Belina", color: "Azul")]
+        completionHandler(.success(cars))
+    }
+}
